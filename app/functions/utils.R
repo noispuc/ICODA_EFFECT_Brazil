79b7f167d154ed5a74320f63574e90a5522ddaac
thaiscamargo <96726143+thaiscamargo@users.noreply.github.com>
2022-01-09 10:44:36 -0300

    Traduzindo as mensagens de erro para inglês

--- app/functions/utils.R
+++ app/functions/utils.R
@@ -1,13 +1,13 @@
 # funcao auxiliar de teste para duas variaveis
 up_two <- function(data, mean) {
   if (length(data) > 1 & mean == 'both'){
-    'Erro: Escolha no máximo uma variável para plotar esse tipo de gráfico!'
+    'Error: Choose one variable at maximum!'
   }
   else if (length(data) > 2) {
-    'Erro: Escolha no máximo duas variáveis para plotar esse tipo de gráfico!'
+    'Error: Choose two variable at maximum!'
   }
   else if (length(data) == 0) {
-    'Erro: Escolha no mínimo uma variável para plotar esse tipo de gráfico!'
+    'Error: Choose at least one variable!'
   } 
   else NULL
 }
